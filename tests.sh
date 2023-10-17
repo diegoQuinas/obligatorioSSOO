4f866125803769adb165127cf9c1aea75b76102d
Diego Perez Giordan <diegoperezgiordan@gmail.com>
2023-10-17 15:55:53 -0300

    mejorados test automaticos

--- tests.sh
+++ tests.sh
@@ -12,61 +12,17 @@ else
 fi
 
 
-save_bad_output() {
-  echo "=====================" >> "$bad_output_file"
-  echo "$1" >> "$bad_output_file"
-  echo "=====================" >> "$bad_output_file"
-}
-
 check_output(){
-  bad_output=false
   # Verificar que D nunca va antes de B ni E antes de C ni G antes de H
-  if grep -q 'D.*B\|E.*C\|G.*H' <<< "$output" || grep -q 'F.*D\|F.*E\|G.*E\|G.*H' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que Q nunca va antes de F o G
-  if grep -q 'Q.*F\|Q.*G' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que I, J, y K nunca van antes de Q
-  if grep -q 'I.*Q\|J.*Q\|K.*Q' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que L nunca va antes de I
-  if grep -q 'L.*I' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que M nunca va antes de I o J
-  if grep -q 'M.*I\|M.*J' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que N nunca va antes de L
-  if grep -q 'N.*L' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que O nunca va antes de P
-  if grep -q 'O.*P' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Verificar que P nunca va antes de M
-  if grep -q 'P.*M' <<< "$output"; then
-    bad_output=true # Bandera para indicar que esta mal el output
-  fi
-
-  # Si se encontraron errores, mostrar un mensaje
-  if $bad_output; then
-    save_bad_output $output
-    echo "Orden incorrecto. Se ha guardado en $bad_output_file."
-  else
-    echo "Orden correcto."
-  fi
+  cat "$output" | grep --color=always 'D.*B\|E.*C\|G.*H' 
+  cat "$output" | grep --color=always 'F.*D\|F.*E\|G.*E\|G.*H' 
+  cat "$output" | grep --color=always 'Q.*F\|Q.*G' 
+  cat "$output" | grep --color=always 'I.*Q\|J.*Q\|K.*Q'
+  cat "$output" | grep --color=always 'L.*I' 
+  cat "$output" | grep --color=always 'M.*I\|M.*J'
+  cat "$output" | grep --color=always 'N.*L'
+  cat "$output" | grep --color=always 'O.*P' 
+  cat "$output" | grep --color=always 'P.*M'
 }
 
 # Compila el programa
@@ -81,6 +37,7 @@ if [ $? -eq 0 ]; then
     # Ejecuta el programa y guarda la salida estándar en el archivo
     ./ejercicio2 > $output
 
+    echo "============================="
     # Verifica si la ejecución tuvo éxito
     if [ $? -eq 0 ]; then
       echo "Ejecución $i"
